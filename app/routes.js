e550eb13012d2804729588a1d231d55a3f0f16f7
Brian Bier <briansbier@gmail.com>
2016-07-26 22:07:23 -0400

    add twitter and facebook oauth

--- app/routes.js
+++ app/routes.js
@@ -50,6 +50,33 @@ module.exports = function(app,passport){
         });
     });
 
+    // =====================================
+    // FACEBOOK ROUTES =====================
+    // =====================================
+    // route for facebook authentication and login
+    app.get('/auth/facebook', passport.authenticate('facebook', { scope : 'email' }));
+
+    // handle the callback after facebook has authenticated the user
+    app.get('/auth/facebook/callback',
+        passport.authenticate('facebook', {
+            successRedirect : '/profile',
+            failureRedirect : '/'
+        }));
+    // =====================================
+    // TWITTER ROUTES =====================
+    // =====================================
+    // route for twitter authentication and login
+    app.get('/auth/twitter', passport.authenticate('twitter'));
+
+    // handle the callback after twitter has authenticated the user
+    app.get('/auth/twitter/callback',
+        passport.authenticate('twitter', {
+            successRedirect : '/profile',
+            failureRedirect : '/'
+        }));
+
+
+
 
     // =====================================
     // LOGOUT ==============================
---
+++ config/auth.js
@@ -0,0 +1,1 @@
+// expose our config directly to our application using module.exports
